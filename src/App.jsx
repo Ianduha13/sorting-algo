123d20c044393688cf46f1cedc52a61dc31fa7e8
Ian Duhamel <ianduhamelhayes@gmail.com>
2023-03-09 18:07:14 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Dashboard from "./screens/Dashboard"
+const App = () => {
+	return (
+		<div className='App'>
+			<Dashboard />
+		</div>
+	)
+}
+
+export default App
---
+++ src/screens/Dashboard.jsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+	return (
+		<section>
+			<div></div>
+		</section>
+	)
+}
+export default Dashboard
